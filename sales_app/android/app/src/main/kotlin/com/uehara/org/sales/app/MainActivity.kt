35af588d531f1a1659aaecee88e600c4e7e4ccac
Rodrigo Uehara <r.uehara19@gmail.com>
2024-11-19 18:09:21 -0300

    Initial commit

---
+++ sales_app/android/app/src/main/kotlin/com/uehara/org/sales/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.uehara.org.sales_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ sales_app/lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:sales_app/app/app.dart';
+import 'package:sales_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
