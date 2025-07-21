46f8775f6029637aff6435b4b9ce15be6dd5dc68
Rodrigo <r.uehara19@gmail.com>
2025-07-21 19:45:14 -0300

    feat: Update theme configuration with new seed color and ensure system colors are used

--- packages/theme_package/lib/src/core/app_themes.dart
+++ packages/theme_package/lib/src/core/app_themes.dart
@@ -7,9 +7,11 @@ class AppThemes {
   static ThemeData _buildTheme(Brightness brightness) {
     return ThemeData(
       useMaterial3: true,
+      useSystemColors: true,
+      visualDensity: VisualDensity.compact,
       brightness: brightness,
       colorScheme: ColorScheme.fromSeed(
-        seedColor: Colors.blue,
+        seedColor: const Color.fromARGB(255, 2, 1, 92),
         brightness: brightness,
       ),
       appBarTheme: const AppBarTheme(centerTitle: true, elevation: 0),
